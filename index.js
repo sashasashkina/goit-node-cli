908ba0cb3b313ab115d59ea3150eecaa06bcef4c
sashasashkina <koshyl.apps@gmail.com>
2024-03-13 21:11:41 +0200

    commitseven

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ async function invokeAction({ action, id, name, email, phone }) {
 
     case "remove":
       const deleteContact = await removeContact(id);
-      return console.log(deleteContact);
+      return console.table(deleteContact);
       break;
 
     default:
